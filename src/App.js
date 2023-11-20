1eada53bb92587b47dd00f75b6b67e025baf8eb9
DanyalNazir10 <danyal.nazir@mergestack.com>
2023-11-19 22:34:34 -0800

    Added map container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import "leaflet/dist/leaflet.css"
+import { MapContainer, TileLayer } from 'react-leaflet';
 
 function App() {
-
+return(
+<MapContainer center={[31.5204, 74.3587]} zoom={12}>
+  <TileLayer
+  attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+  />
+</MapContainer>
+);
 }
 
 export default App;
