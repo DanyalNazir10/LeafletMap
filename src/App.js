5e35c089115ca1162ff19ce60da9c79360c69dcb
DanyalNazir10 <danyal.nazir@mergestack.com>
2023-11-19 22:52:00 -0800

    Added markers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import './App.css';
-import "leaflet/dist/leaflet.css"
-import { MapContainer, TileLayer } from 'react-leaflet';
+import "./App.css";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { Icon } from "leaflet";
+import markerImg from "../src/assets/images/marker.png";
 
 function App() {
-return(
-<MapContainer center={[31.5204, 74.3587]} zoom={12}>
-  <TileLayer
-  attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-  />
-</MapContainer>
-);
+  const markers = [
+    {
+      geocode: [31.4805, 74.3239],
+      popUp: "PopUp from model town",
+    },
+    {
+      geocode: [31.4697, 74.2728],
+      popUp: "PopUp from johar town",
+    },
+    {
+      geocode: [31.5165, 74.3499],
+      popUp: "PopUp from gulberg",
+    },
+  ];
+  const customMrakerIcon = new Icon({
+    iconUrl: markerImg,
+    iconSize: [38, 38],
+  });
+  return (
+    <MapContainer center={[31.5204, 74.3587]} zoom={12}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {markers.map((marker) => (
+        <Marker position={marker.geocode} icon={customMrakerIcon} />
+      ))}
+    </MapContainer>
+  );
 }
 
 export default App;
